d07bdd41825fdcea561e6ec6645e1c71b0707cbd
Lawrence <44716177+laoyizun@users.noreply.github.com>
2023-07-25 00:07:02 +0800

    Update tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -24,6 +24,18 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16,sprites.castle.tileGrass2], TileScale.Sixteen);
+            case "dz_级别2":
+            case "dz_级别2":return tiles.createTilemap(hex`0a0008000101010101010101010101020202020202020201010202020202020202010102020202020202020101020202020202020201010402050206020202010102020202020202020101010103030303010101`, img`
+2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . 2 
+2 . . . . . . . . 2 
+2 . . . . . . . . 2 
+2 . . . . . . . . 2 
+2 . . . . . . . . 2 
+2 . . . . . . . . 2 
+2 2 2 . . . . 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.floorLight0,sprites.dungeon.darkGroundCenter,sprites.castle.tileGrass3,myTiles.dz_tile8,myTiles.dz_tile10,myTiles.dz_tile11], TileScale.Sixteen);
+        
         }
         return null;
     })
@@ -31,6 +43,12 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "dz_myTile2":
+            case "dz_tile11":return dz_tile11;
+            case "dz_myTile":
+            case "dz_tile8":return dz_tile8;
+            case "dz_myTile1":
+            case "dz_tile10":return dz_tile10;
         }
         return null;
     })
