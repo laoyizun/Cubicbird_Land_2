26a8c1ac2d1409bb8e6d2cbe646440bfaa0a0d83
Lawrence <44716177+laoyizun@users.noreply.github.com>
2023-07-25 12:50:08 +0800

    Update tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -16,6 +16,64 @@ namespace myTiles {
     export const zhr_tile2 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const zhr_tile3 = image.ofBuffer(hex``);
+        //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile17 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile19 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile20 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile21 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile22 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile23 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile25 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile18 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile24 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile27 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile26 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zyx_tile28 = image.ofBuffer(hex``);
+
+    
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "级别2":
@@ -67,7 +125,36 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16,sprites.dungeon.floorLight2,sprites.dungeon.hazardWater,myTiles.zhr_tile8,myTiles.zhr_tile1,myTiles.zhr_tile2,myTiles.zhr_tile3], TileScale.Sixteen);
- }
+            case "zyx_级别3":
+            case "zyx_级别3":return tiles.createTilemap(hex`14000a000706060606060606060606060606060606060605040606060606060606060606060606060606060502010101010101010101010101010101010101030201010101010101010101010101010101010103020101010101010101010101010101010101010302010101010101010101010101010101010101030201010101010101010101010101010101010103020101010101010101010101010101010101010302010b010c010101010101010101010101010103080909090909090909090909090909090909090a`, img`
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.zyx_tile14,myTiles.zyx_tile15,myTiles.zyx_tile16,myTiles.zyx_tile20,myTiles.zyx_tile21,myTiles.zyx_tile23,myTiles.zyx_tile25,myTiles.zyx_tile12,myTiles.zyx_tile18,myTiles.zyx_tile24,myTiles.zyx_tile27,myTiles.zyx_tile26], TileScale.Sixteen);
+            case "zyx_级别2":
+            case "zyx_级别2":return tiles.createTilemap(hex`19000a000101010106070708010101010113121212121212121212121101010101060a0a080101010101101212121212121212121211010101010903040901010101010e0d0d0d0d0d0d0d0d0d0d0f010101010502020501010101010e0d0d0d0d0d0d0d0d0d0d0f010101010102020101010101010e0d0d0d0d0d140d0d0d0d0f010101010102020101010101010e0d0d0d0d0d0d0d0d0d0d0f01010101010c0c0101010101010e0d0d0d0d0d0d0d0d0d0d0f010101010102020101010101010e0d0d0d0d0d0d0d0d0d0d0f0101010101010b01010101010115161616161616161616161701010101010101010101010101010101010101010101010101`, img`
+. . . . 2 2 2 2 . . . . . . . . . . . . . . . . . 
+. . . . 2 . . 2 . . . . . . . . . . . . . . . . . 
+. . . . 2 . . 2 . . . . . . . . . . . . . . . . . 
+. . . . 2 . . 2 . . . . . . . . . . . . . . . . . 
+. . . . 2 . . 2 . . . . . . . . . . . . . . . . . 
+. . . . 2 . . 2 . . . . . . . . . . . . . . . . . 
+. . . . 2 . . 2 . . . . . . . . . . . . . . . . . 
+. . . . 2 . . 2 . . . . . . . . . . . . . . . . . 
+. . . . 2 . . 2 . . . . . . . . . . . . . . . . . 
+. . . 2 . . . . 2 . . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.zyx_tile1,myTiles.zyx_tile2,myTiles.zyx_tile4,myTiles.zyx_tile5,myTiles.zyx_tile3,myTiles.zyx_tile6,myTiles.zyx_tile7,myTiles.zyx_tile8,myTiles.zyx_tile9,myTiles.zyx_tile10,myTiles.zyx_tile11,myTiles.zyx_tile13,myTiles.zyx_tile14,myTiles.zyx_tile15,myTiles.zyx_tile16,myTiles.zyx_tile20,myTiles.zyx_tile21,myTiles.zyx_tile23,myTiles.zyx_tile25,myTiles.zyx_tile28,myTiles.zyx_tile12,myTiles.zyx_tile18,myTiles.zyx_tile24], TileScale.Sixteen);
+        
+        
+        
+        }
         return null;
     })
 
@@ -89,6 +176,64 @@ namespace myTiles {
             case "zhr_tile2":return zhr_tile2;
             case "zhr_myTile2":
             case "zhr_tile3":return zhr_tile3;
+
+
+            case "zyx_myTile0":
+		    case "zyx_tile2":return zyx_tile2;
+		    case "zyx_myTile1":
+	        case "zyx_tile3":return zyx_tile3;
+	        case "zyx_myTile2":
+	        case "zyx_tile4":return zyx_tile4;
+	        case "zyx_myTile3":
+		    case "zyx_tile5":return zyx_tile5;
+	        case "zyx_myTile4":
+	        case "zyx_tile6":return zyx_tile6;
+		    case "zyx_myTile5":
+		    case "zyx_tile7":return zyx_tile7;
+		            case "zyx_myTile6":
+		            case "zyx_tile8":return zyx_tile8;
+		            case "zyx_myTile7":
+		            case "zyx_tile9":return zyx_tile9;
+		            case "zyx_myTile8":
+		            case "zyx_tile10":return zyx_tile10;
+		            case "zyx_myTile9":
+		            case "zyx_tile11":return zyx_tile11;
+		            case "zyx_myTile":
+		            case "zyx_tile1":return zyx_tile1;
+		            case "zyx_myTile11":
+		            case "zyx_tile13":return zyx_tile13;
+		            case "zyx_myTile12":
+		            case "zyx_tile14":return zyx_tile14;
+		            case "zyx_myTile13":
+		            case "zyx_tile15":return zyx_tile15;
+		            case "zyx_myTile14":
+		            case "zyx_tile16":return zyx_tile16;
+		            case "zyx_myTile15":
+		            case "zyx_tile17":return zyx_tile17;
+		            case "zyx_myTile17":
+		            case "zyx_tile19":return zyx_tile19;
+		            case "zyx_myTile18":
+		            case "zyx_tile20":return zyx_tile20;
+		            case "zyx_myTile19":
+		            case "zyx_tile21":return zyx_tile21;
+		            case "zyx_myTile20":
+		            case "zyx_tile22":return zyx_tile22;
+		            case "zyx_myTile21":
+		            case "zyx_tile23":return zyx_tile23;
+		            case "zyx_myTile23":
+		            case "zyx_tile25":return zyx_tile25;
+		            case "zyx_myTile10":
+		            case "zyx_tile12":return zyx_tile12;
+		            case "zyx_myTile16":
+		            case "zyx_tile18":return zyx_tile18;
+		            case "zyx_myTile22":
+		            case "zyx_tile24":return zyx_tile24;
+		            case "zyx_myTile25":
+		            case "zyx_tile27":return zyx_tile27;
+		            case "zyx_myTile24":
+		            case "zyx_tile26":return zyx_tile26;
+		            case "zyx_myTile26":
+		            case "zyx_tile28":return zyx_tile28;
         }
         return null;
     })
