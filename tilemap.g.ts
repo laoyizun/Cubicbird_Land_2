9527542a20947962c1ff6af85d939727437d16ad
Lawrence <44716177+laoyizun@users.noreply.github.com>
2023-07-25 11:54:44 +0800

    Update tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -8,7 +8,14 @@ namespace myTiles {
     export const tile8 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile10 = image.ofBuffer(hex``);
-
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zhr_tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zhr_tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zhr_tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const zhr_tile3 = image.ofBuffer(hex``);
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "级别2":
@@ -41,7 +48,26 @@ namespace myTiles {
 2 . . . . . . . . 2 
 2 2 2 . . . . 2 2 2 
 `, [myTiles.transparency16,sprites.dungeon.floorLight0,sprites.dungeon.darkGroundCenter,sprites.castle.tileGrass3,myTiles.tile8,myTiles.tile10,myTiles.tile11], TileScale.Sixteen);
-        }
+            case "zhr_级别2":
+            case "zhr_级别2":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010104050603030303030303030303030303010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010102020202010101010101`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.floorLight2,sprites.dungeon.hazardWater,myTiles.tile8,myTiles.tile1,myTiles.tile2,myTiles.tile3], TileScale.Sixteen);
+ }
         return null;
     })
 
@@ -54,6 +80,15 @@ namespace myTiles {
             case "tile8":return tile8;
             case "myTile1":
             case "tile10":return tile10;
+
+            case "zhr_myTile":
+            case "zhr_tile8":return zhr_tile8;
+            case "zhr_myTile0":
+            case "zhr_tile1":return zhr_tile1;
+            case "zhr_myTile1":
+            case "zhr_tile2":return zhr_tile2;
+            case "zhr_myTile2":
+            case "zhr_tile3":return zhr_tile3;
         }
         return null;
     })
