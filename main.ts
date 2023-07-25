87ecc7dd5c7c82ad61f47c7474526a17ddbea801
Lawrence <44716177+laoyizun@users.noreply.github.com>
2023-07-25 13:56:00 +0800

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -7,4 +7,12 @@ scene_util.captureRegisteringRoomScenes(() => {
     zhr_room.prepare_zhr_room()
     game.popScene()
 })
+scene_util.captureRegisteringRoomScenes(() => {
+    zyx_room.prepare_zyx_room()
+    game.popScene()
+})
+scene_util.captureRegisteringRoomScenes(() => {
+    xcx_room.prepare_xcx_room()
+    game.popScene()
+})
 cbland.startVillage(false)
