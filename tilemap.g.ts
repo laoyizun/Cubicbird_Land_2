76a3d909c2ca7e91131beb36d8478d42411e821b
Lawrence <44716177+laoyizun@users.noreply.github.com>
2023-07-27 09:48:45 +0800

    Update tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,12 +2,7 @@
 namespace myTiles {
 	//% fixedInstance jres blockIdentity=images._tile
 	export const transparency16 = image.ofBuffer(hex``);
-	//% fixedInstance jres blockIdentity=images._tile
-	export const tile11 = image.ofBuffer(hex``);
-	//% fixedInstance jres blockIdentity=images._tile
-	export const tile8 = image.ofBuffer(hex``);
-	//% fixedInstance jres blockIdentity=images._tile
-	export const tile10 = image.ofBuffer(hex``);
+
 	//% fixedInstance jres blockIdentity=images._tile
 	export const zhr_tile8 = image.ofBuffer(hex``);
 	//% fixedInstance jres blockIdentity=images._tile
@@ -109,17 +104,6 @@ namespace myTiles {
 
 	helpers._registerFactory("tilemap", function (name : string) {
 		switch (helpers.stringTrim(name)) {
-			case "dz_级别2":
-			case "dz_级别2": return tiles.createTilemap(hex`0a0008000101010101010101010101020202020202020201010202020202020202010102020202020202020101020202020202020201010402050206020202010102020202020202020101010103030303010101`, img`
-2 2 2 2 2 2 2 2 2 2 
-2 . . . . . . . . 2 
-2 . . . . . . . . 2 
-2 . . . . . . . . 2 
-2 . . . . . . . . 2 
-2 . . . . . . . . 2 
-2 . . . . . . . . 2 
-2 2 2 . . . . 2 2 2 
-`, [myTiles.transparency16, sprites.dungeon.floorLight0, sprites.dungeon.darkGroundCenter, sprites.castle.tileGrass3, myTiles.tile8, myTiles.tile10, myTiles.tile11], TileScale.Sixteen);
 			case "zhr_级别2":
 			case "zhr_级别2": return tiles.createTilemap(hex`1000100001010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010104050607080d0303030303030303030301010101010101010101010101010101010101010101010101010101010101090101010101010101010101010101010a0101010101010101010101010101010b01010101010101010101010101010101010101010101010101010101010101090101010101010101010101010101010a0101010101010101010101010101010c01010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010102020202010101010101`, img`
 . . . . . . . . . . . . . . . . 
@@ -221,13 +205,6 @@ namespace myTiles {
 	helpers._registerFactory("tile", function (name : string) {
 		switch (helpers.stringTrim(name)) {
 			case "transparency16": return transparency16;
-			case "myTile2":
-			case "tile11": return tile11;
-			case "myTile":
-			case "tile8": return tile8;
-			case "myTile1":
-			case "tile10": return tile10;
-			case "myTile":
 			case "zhr_tile8": return zhr_tile8;
 			case "myTile0":
 			case "zhr_tile1": return zhr_tile1;
